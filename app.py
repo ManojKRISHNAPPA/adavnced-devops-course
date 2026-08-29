d54e67eb33cfe79e5295c3d6dc33cd619330a7d5
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2026-08-29 20:02:42 +0530

    changed to groq models

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import os
 
-from langchain_community.llms import Ollama
+from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -11,30 +10,95 @@ st.set_page_config(
     page_icon="🚨"
 )
 
-st.title("Ollama ChatBot")
+st.title("Groq ChatBot")
 
-temperature = st.slider("Temperature", 0.0, 1.0, 0.7)
-max_tokens = st.slider("Max Tokens", 100, 1000, 100)
+# -----------------------------
+# Sidebar
+# -----------------------------
+with st.sidebar:
+    st.header("🔐 Groq Configuration")
 
-input_text = st.text_input("Ask you questions..?")
+    groq_api_key = st.text_input(
+        "Enter Groq API Key",
+        type="password",
+        placeholder="gsk_..."
+    )
 
+    model = st.selectbox(
+        "Select Model",
+        [
+            "openai/gpt-oss-120b",
+            "qwen/qwen3.8-27b"
+        ]
+    )
+
+    temperature = st.slider(
+        "Temperature",
+        0.0,
+        1.0,
+        0.7
+    )
+
+    max_tokens = st.slider(
+        "Max Tokens",
+        100,
+        2000,
+        500
+    )
+
+
+# -----------------------------
+# Prompt
+# -----------------------------
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","you are helpfull assitant."),
-        ("user","Question: {question} ")
+        (
+            "system",
+            "You are a helpful assistant."
+        ),
+        (
+            "user",
+            "Question: {question}"
+        )
     ]
 )
 
 
-llm = Ollama(
-    temperature=temperature,
-    model="llama3.1:latest"
-)
+# -----------------------------
+# Initialize Groq
+# -----------------------------
+if groq_api_key:
 
-output_parser = StrOutputParser()
-chain  = prompt| llm | output_parser
+    llm = ChatGroq(
+        api_key=groq_api_key,
+        model=model,
+        temperature=temperature,
+        max_tokens=max_tokens
+    )
 
+    output_parser = StrOutputParser()
 
-if input_text:
-    response = chain.invoke({"question":input_text})
-    st.write(response)
+    chain = prompt | llm | output_parser
+
+    # -----------------------------
+    # User Input
+    # -----------------------------
+    input_text = st.text_input(
+        "Ask your question..?"
+    )
+
+    if input_text:
+
+        with st.spinner("Thinking..."):
+
+            response = chain.invoke(
+                {
+                    "question": input_text
+                }
+            )
+
+        st.write(response)
+
+else:
+
+    st.info("👈 Please enter your Groq API key in the sidebar.")
